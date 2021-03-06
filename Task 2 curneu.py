71b9a036c598272dc2665e3568a7ea955c06560d
YogeshwaranJ <yogeshwaranjeevanandham@gmail.com>
2021-03-06 23:47:17 +0530

    Update Task 2 curneu.py

--- Task 2 curneu.py
+++ Task 2 curneu.py
@@ -68,6 +68,68 @@ diabetes_dat.dropna(subset =['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThi
        'BMI', 'DiabetesPedigreeFunction', 'Age'] , inplace=True)
 diabetes_dat.info(verbose=True)
 
+#selecting the features and seperating features and target variables
+features = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin',
+       'BMI', 'DiabetesPedigreeFunction', 'Age']
+X = diabetes_dat[features]
+y = diabetes_dat.iloc[:,8]
 
+
+#splitting of dataset
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.20)
+
+
+#standardizing the training and testing data
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+
+
+#function to calculate euclidean distance
+def euclid_dist(X1,X2):
+    dist = np.sum((X1 - X2)**2)
+    return np.sqrt(dist)
+
+#function of knn predict
+def knn_predict(X_train, X_test, y_train, y_test, k):
+    
+    # Counter to help with label voting
+    from collections import Counter
+    
+    # Make predictions on the test data
+    # Need output of 1 prediction per test data point
+    y_hat_test = []
+
+    for test_point in X_test:
+        distances = []
+
+        for train_point in X_train:
+            distance = euclid_dist(test_point, train_point)
+            distances.append(distance)
+        
+        # Store distances in a dataframe
+        df_dists = pd.DataFrame(data=distances, columns=['dist'], 
+                                index=y_train.index)
+        
+        # Sort distances, and only consider the k closest points
+        df_nn = df_dists.sort_values(by=['dist'], axis=0)[:k]
+
+        # Create counter object to track the labels of k closest neighbors
+        counter = Counter(y_train[df_nn.index])
+
+        # Get most common label of all the nearest neighbors
+        prediction = counter.most_common()[0][0]
+        
+        # Append prediction to output list
+        y_hat_test.append(prediction)
+        
+    return y_hat_test
+
+
+# Make predictions on test dataset
+y_hat_test = knn_predict(X_train, X_test, y_train, y_test, k=20)
 
     
