a78287ecca80469364fd1830446a7db85aa3decb
YogeshwaranJ <yogeshwaranjeevanandham@gmail.com>
2021-03-06 20:59:08 +0530

    Update Task 2 curneu.py

--- Task 2 curneu.py
+++ Task 2 curneu.py
@@ -9,6 +9,11 @@ Created on Sat Mar  6 18:31:20 2021
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import metrics
+from scipy.stats import mode 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split 
+
 
 #reading the data using pandas
 diabetes_dat = pd.read_csv('C:/Users/yoges/Documents/SD03Q03/SD03Q03/Diabetes Database.csv')
