3f959e2af6b120e19d635f268be758add761d4af
YogeshwaranJ <yogeshwaranjeevanandham@gmail.com>
2021-03-06 20:57:30 +0530

    Create Task 2 curneu.py

---
+++ Task 2 curneu.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 18:31:20 2021
+
+@author: yoges
+"""
+
+#importing all the needed libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#reading the data using pandas
+diabetes_dat = pd.read_csv('C:/Users/yoges/Documents/SD03Q03/SD03Q03/Diabetes Database.csv')
+
